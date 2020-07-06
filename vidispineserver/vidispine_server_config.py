f767a6400a8276a480979e10c9cd99abba8b2ef6
Marcus Pennington <marcuspen@msn.com>
2020-07-06 16:14:23 +0100

    Fix server config yaml

--- vidispineserver/vidispine_server_config.py
+++ vidispineserver/vidispine_server_config.py
@@ -1,14 +1,23 @@
+import os
 import yaml
-server_config_file = '/etc/vidispine/server.yaml'
-stream = open(server_config_file, 'r')
+
+SERVER_CONFIG_FILE = '/etc/vidispine/server.yaml'
+stream = open(SERVER_CONFIG_FILE, 'r')
 server_config = yaml.load(stream)
 
+POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+VIDISPINE_DB_NAME = os.getenv('VIDISPINE_DB_NAME')
+VIDISPINE_DB_USER = os.getenv('VIDISPINE_DB_USER')
+VIDISPINE_DB_PSWD = os.getenv('VIDISPINE_DB_PSWD')
 
 server_config['database'] = server_config.get('database', {})
-server_config['database']['url'] = 'jdbc:postgresql://${POSTGRES_HOST}/${VIDISPINE_DB_NAME}'  # NOQA
-server_config['database']['user'] = '${VIDISPINE_DB_USER}'
-server_config['database']['password'] = '${VIDISPINE_DB_PSWD}'
+server_config['database']['url'] = 'jdbc:postgresql://{}/{}'.format(
+    POSTGRES_HOST, VIDISPINE_DB_NAME
+)
+
+server_config['database']['user'] = VIDISPINE_DB_USER
+server_config['database']['password'] = VIDISPINE_DB_PSWD
 
 
-with open(server_config_file, 'w') as yaml_file:
+with open(SERVER_CONFIG_FILE, 'w') as yaml_file:
     yaml_file.write(yaml.dump(server_config, default_flow_style=False))
